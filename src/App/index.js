da8aa47920dfd8d4b953e207578367bb82d0f5b6
Kristian Rosland <kristian.rosland@gmail.com>
2018-01-03 18:42:26 +0100

    Commit

--- src/App/index.js
+++ src/App/index.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
 import logo from '../logo.svg';
-import CounterComponent from './CounterComponent';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <CounterComponent />
+      <div> App </div>
     );
   }
 }
