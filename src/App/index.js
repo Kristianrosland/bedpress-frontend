ba5857856c0bb36c4adcf1dde05bdc5e91c476dd
andrmos <mossige.a@gmail.com>
2018-01-03 18:41:50 +0100

    WIP: Firebase Authenticator

--- src/App/index.js
+++ src/App/index.js
@@ -2,11 +2,43 @@ import React, { Component } from 'react';
 import logo from '../logo.svg';
 import CounterComponent from './CounterComponent';
 import './App.css';
+import { FirebaseAuth } from 'react-firebaseui';
+import firebase from 'firebase';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { singedIn: false };
+  }
+
   render() {
+    const uiConfig = {
+      // signInFlow: 'redirect',
+      // signInSuccessUrl: '/signIn',
+      signInOptions: [firebase.auth.EmailAuthProvider.PROVIDER_ID],
+      tosUrl: 'www.fagutvalget.no',
+      credentialHelper: 'firebaseui.auth.CredentialHelper.NONE',
+      callbacks: {
+        signInSuccess: () => {
+          this.setState({ signedIn: true });
+          return true; // Avoid redirects after sign-in.
+        }
+      }
+    };
+
+    console.log(this.state.signedIn);
+
     return (
-      <CounterComponent />
+      <div>
+      {
+        !this.state.singedIn &&
+          <FirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+      }
+      {
+        this.state.singedIn &&
+          <p> YES </p>
+      }
+      </div>
     );
   }
 }
