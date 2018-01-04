5ec68e604eb3675047ba1400874818b6d3d8c65a
Kristian Rosland <kristian.rosland@gmail.com>
2018-01-04 15:57:34 +0100

    Add default route / home (App)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
@@ -8,6 +9,7 @@ import { Provider } from 'react-redux';
 import reducers from './reducers';
 import thunkMiddleware from 'redux-thunk';
 import firebase from 'firebase/app';
+
 
 var config = {
   apiKey: "AIzaSyDiKrfHJq-FYHoHE13jPtMCbWs1N2hKDCg",
@@ -23,7 +25,9 @@ const store = createStore(reducers, undefined, applyMiddleware(thunkMiddleware))
 
 const MyApp = () => (
   <Provider store={store}>
-    <App />
+    <Router>
+      <Route path="/:filter?" component={App} />
+    </Router>
   </Provider>
 );
 
