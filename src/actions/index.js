8590e4d0d6a42575b6cd46834b61f31800958c4c
andrmos <mossige.a@gmail.com>
2018-01-04 13:57:27 +0100

    Remove unused counter actions and reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,15 +1,3 @@
-export function increment() {
-  return {
-    type: 'INCREMENT'
-  }
-}
-
-export function reset() {
-  return {
-    type: 'RESET'
-  }
-}
-
 export function authSuccess(user) {
   return {
     type: 'AUTH_SUCCESS',
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,10 +1,8 @@
 import { combineReducers } from 'redux';
-import countReducer from './countReducer';
 import authReducer from './authReducer';
 
 const reducers = combineReducers({
-  counter: countReducer,
-  auth: authReducer,
+  auth: authReducer
 });
 
 export default reducers;
