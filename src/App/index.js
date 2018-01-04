b528bdac073a80029c6208f92d635dbdf7a4dfb4
Kristian Rosland <kristian.rosland@gmail.com>
2018-01-04 01:14:57 +0100

    Display loading instead of sign in while waiting to check if user is authenticated on page reload

--- src/App/index.js
+++ src/App/index.js
@@ -12,6 +12,8 @@ class App extends Component {
     firebase.auth().onAuthStateChanged(function(user) {
         if (user) {
           that.props.dispatch({ type: 'LOGGED_IN', user: user })
+        } else {
+          that.props.dispatch({ type: 'NO_AUTH' }) //Signal that the user is not authenticated
         }
     });
   }
@@ -26,15 +28,17 @@ class App extends Component {
       credentialHelper: firebaseui.auth.CredentialHelper.GOOGLE_YOLO,
     };
 
+    const loading = this.props.loading ? <p> Loading.. </p> : null;
+    const displayName = this.props.user ? <p> { this.props.user.displayName } </p> : null;
+    const loginScreen = !this.props.user && !this.props.loading
+      ? <FirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+      : null;
+
     return (
       <div>
-        {
-          !this.props.user &&
-            <FirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
-        }
-        {
-          this.props.user && <p> { this.props.user.displayName } </p>
-        }
+        { loading }
+        { displayName }
+        { loginScreen }
       </div>
     );
   }
@@ -49,6 +53,7 @@ const mapDispatchToProps = dispatch => {
 const mapStateToProps = state => {
   console.log(state)
   return {
+    loading: state.auth.loadingUser,
     user: state.auth.user,
   }
 }
--- src/reducers/authReducer.js
+++ src/reducers/authReducer.js
@@ -1,10 +1,17 @@
-export default function authReducer(state = {}, action) {
+export default function authReducer(state = { loadingUser: true }, action) {
   switch(action.type) {
     case 'LOGGED_IN':
       return {
         ...state,
         user: action.user,
+        loadingUser: false,
       };
+    case 'NO_AUTH':
+      return {
+        ...state,
+        user: undefined,
+        loadingUser: false,
+      }
     default:
       break;
   }
