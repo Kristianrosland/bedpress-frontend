acd5f5173cbb5822cd1d090228b8a0cc902cbf47
Kristian Rosland <kristian.rosland@gmail.com>
2018-01-05 00:09:33 +0100

    Add header button with ref to MinProfil'

--- src/App/Header/index.js
+++ src/App/Header/index.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
+import HeaderButton from './HeaderButton';
+import header from './header.png';
 import './header.css';
-import header from './header.png'
 
 class Header extends Component {
   render() {
+    const myProfileButton = (
+      <HeaderButton
+        icon='user'
+        text='MIN PROFIL'
+        route='myProfile' />);
+
     return (
       <div className='header'>
         <img src={header} alt='Header' className='header-logo'/>
+        { myProfileButton }
       </div>
     );
   }
