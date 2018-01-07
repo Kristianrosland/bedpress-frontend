cb0cc408f8944e21f2f8364d07599ddf2da660ed
Kristian Rosland <kristian.rosland@gmail.com>
2018-01-07 03:00:04 +0100

    Add styling to myProfile

--- src/App/MyProfilePage/index.js
+++ src/App/MyProfilePage/index.js
@@ -8,18 +8,21 @@ import './myProfilePage.css';
 
 class MyProfilePage extends Component {
   render() {
-    const newUser = this.props.newUser ? <p> {'Welcome new user!'} </p> : null;
-    const allergies = [ 'Gluten', 'Vegetarianer', 'Pesketarianer' ]
-    const saveButton = <button className='save-button'> Lagre </button>;
+    const newUser = this.props.newUser;
+    const allergies = [ 'Gluten', 'Vegetarianer', 'Pesketarianer', 'YES' ]
+    const saveButton = <button className='save-button'> Lagre innstillinger </button>;
 
     return (
-      <div className="container">
-        { newUser }
-        <StudyProgramDropdown />
-        <YearDropdown />
-        <AllergyInputField />
-        <AllergyTags allergies={allergies}/>
-        { saveButton }
+      <div className='outer-container'>
+        <div className='container'>
+          <div className='display-name'> { "Kristian Rosland" } </div>
+          <div className='email'> { "Kristian.rosland@gmail.com" } </div>
+          <StudyProgramDropdown redBorder={newUser} />
+          <YearDropdown redBorder={newUser} />
+          <AllergyInputField />
+          <AllergyTags allergies={allergies}/>
+          { saveButton }
+        </div>
       </div>
     )
   }
